b546a108559d6ce3a0e81a75c55e220bff7f9daf
VivekNair2 <vivekh2004@gmail.com>
2024-12-25 23:53:52 +0530

    data validation done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+
 
 STAGE_NAME="Data ingestion stage"
 
@@ -13,3 +15,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME="Data Validation stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started >>>>>>")
+    data_ingestion=DataValidationTrainingPipeline()
+    data_ingestion.initiate_data_validation()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+except  Exception as e:
+    logger.exception(e)
+    raise e
+    
--- src/datascience/entity/config_entity.py
+++ src/datascience/entity/config_entity.py
@@ -8,3 +8,11 @@ class DataIngestionConfig:
     source_url: str
     local_data_file: Path
     unzip_dir: Path 
+
+
+@dataclass
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir : str
+    all_schemas: dict
