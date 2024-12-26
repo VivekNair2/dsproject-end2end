5e43b68d18b7c6b284bdd8783dc25e503456dc58
VivekNair2 <vivekh2004@gmail.com>
2024-12-26 13:34:51 +0530

    model evaluation done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
-
+from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 STAGE_NAME="Data ingestion stage"
 
 logger.info("welcome to our custom logging data science")
@@ -48,3 +48,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
